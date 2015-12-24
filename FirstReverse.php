633426738b4142addd2c10ffa38fef7db7320f7a
IharPanf <panfilenko.igor@gmail.com>
2015-12-24 21:09:21 +0200

    add FirstReverse.php

---
+++ FirstReverse.php
@@ -0,0 +1,8 @@
+<?php
+//Using the PHP language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+function FirstReverse($str) {  
+  return strrev($str); 
+}
+
+echo  FirstReverse("some string");
+?>
